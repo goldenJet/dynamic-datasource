bcf961e6d2ccde1f8807a802f70c803a38b68fee
goldenJet <505214992@qq.com>
2017-09-04 18:13:15 +0800

    add sql

---
+++ src/main/resources/test1_usr.sql
@@ -0,0 +1,33 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : crx_local
+Source Server Version : 50635
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50635
+File Encoding         : 65001
+
+Date: 2017-09-04 18:11:23
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for usr
+-- ----------------------------
+DROP TABLE IF EXISTS `usr`;
+CREATE TABLE `usr` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of usr
+-- ----------------------------
+INSERT INTO `usr` VALUES ('1', 'test_1', null);
+INSERT INTO `usr` VALUES ('2', 'test_2', null);
